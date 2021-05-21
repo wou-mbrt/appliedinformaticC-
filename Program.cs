7e8859b928ca2771d1fb3e307c2402afc502a587
wou-mbrt <78117064+wou-mbrt@users.noreply.github.com>
2021-05-21 16:08:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab4Mamady
+{
+    public class Graph
+    {
+        private int V; // No. of vertices
+
+        // Array of lists for
+        // Adjacency List Representation
+        private List<int>[] adj;
+
+        // Constructor
+        public Graph(int v)
+        {
+            V = v;
+            adj = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+                adj[i] = new List<int>();
+        }
+
+        // Function to Add an edge into the graph
+        public void AddEdge(int v, int w)
+        {
+            adj[v].Add(w); // Add w to v's list.
+        }
+
+        // A function used by DFS
+        void DFSUtil(int v, bool[] visited)
+        {
+            // Mark the current node as visited
+            // and print it
+            visited[v] = true;
+            Console.Write(v + " ");
+
+            // Recur for all the vertices
+            // adjacent to this vertex
+            List<int> vList = adj[v];
+            foreach (var n in vList)
+            {
+                if (!visited[n])
+                    DFSUtil(n, visited);
+            }
+        }
+
+        // The function to do DFS traversal.
+        // It uses recursive DFSUtil()
+        public void DFS_method(int v)
+        {
+            // Mark all the vertices as not visited
+            // (set as false by default in c#)
+            bool[] visited = new bool[V];
+
+            // Call the recursive helper function
+            // to print DFS traversal
+            DFSUtil(v, visited);
+            Console.WriteLine();
+        }
+
+        public void BFS_method(int s)
+        {
+
+            // Mark all the vertices as not
+            // visited(By default set as false)
+            bool[] visited = new bool[V];
+            for (int i = 0; i < V; i++)
+                visited[i] = false;
+
+            // Create a queue for BFS
+            LinkedList<int> queue = new LinkedList<int>();
+
+            // Mark the current node as
+            // visited and enqueue it
+            visited[s] = true;
+            queue.AddLast(s);
+
+            while (queue.Any())
+            {
+
+                // Dequeue a vertex from queue
+                // and print it
+                s = queue.First();
+                Console.Write(s + " ");
+                queue.RemoveFirst();
+
+                // Get all adjacent vertices of the
+                // dequeued vertex s. If a adjacent
+                // has not been visited, then mark it
+                // visited and enqueue it
+                List<int> list = adj[s];
+                foreach (var val in list)
+                {
+                    if (!visited[val])
+                    {
+                        visited[val] = true;
+                        queue.AddLast(val);
+                    }
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    public class KruskalAlgorithm
+    {
+        public struct Edge
+        {
+            public int Source;
+            public int Destination;
+            public int Weight;
+        }
+
+        public struct Graph
+        {
+            public int VerticesCount;
+            public int EdgesCount;
+            public Edge[] edge;
+        }
+
+        public struct Subset
+        {
+            public int Parent;
+            public int Rank;
+        }
+
+        public static Graph CreateGraph(int verticesCount, int edgesCoun)
+        {
+            Graph graph = new Graph();
+            graph.VerticesCount = verticesCount;
+            graph.EdgesCount = edgesCoun;
+            graph.edge = new Edge[graph.EdgesCount];
+
+            return graph;
+        }
+
+        private static int Find(Subset[] subsets, int i)
+        {
+            if (subsets[i].Parent != i)
+                subsets[i].Parent = Find(subsets, subsets[i].Parent);
+
+            return subsets[i].Parent;
+        }
+
+        private static void Union(Subset[] subsets, int x, int y)
+        {
+            int xroot = Find(subsets, x);
+            int yroot = Find(subsets, y);
+
+            if (subsets[xroot].Rank < subsets[yroot].Rank)
+                subsets[xroot].Parent = yroot;
+            else if (subsets[xroot].Rank > subsets[yroot].Rank)
+                subsets[yroot].Parent = xroot;
+            else
+            {
+                subsets[yroot].Parent = xroot;
+                ++subsets[xroot].Rank;
+            }
+        }
+
+        private static void Print(Edge[] result, int e)
+        {
+            for (int i = 0; i < e; ++i)
+                Console.WriteLine("{0} -- {1} == {2}", result[i].Source, result[i].Destination, result[i].Weight);
+        }
+
+        public static void Kruskal(Graph graph)
+        {
+            int verticesCount = graph.VerticesCount;
+            Edge[] result = new Edge[verticesCount];
+            int i = 0;
+            int e = 0;
+
+            Array.Sort(graph.edge, delegate (Edge a, Edge b)
+            {
+                return a.Weight.CompareTo(b.Weight);
+            });
+
+            Subset[] subsets = new Subset[verticesCount];
+
+            for (int v = 0; v < verticesCount; ++v)
+            {
+                subsets[v].Parent = v;
+                subsets[v].Rank = 0;
+            }
+
+            while (e < verticesCount - 1)
+            {
+                Edge nextEdge = graph.edge[i++];
+                int x = Find(subsets, nextEdge.Source);
+                int y = Find(subsets, nextEdge.Destination);
+
+                if (x != y)
+                {
+                    result[e++] = nextEdge;
+                    Union(subsets, x, y);
+                }
+            }
+
+            Print(result, e);
+        }
+    }
+    class GFG
+    {
+        // A utility function to find the
+        // vertex with minimum distance
+        // value, from the set of vertices
+        // not yet included in shortest
+        // path tree
+        static int V = 9;
+        int minDistance(int[] dist,
+                        bool[] sptSet)
+        {
+            // Initialize min value
+            int min = int.MaxValue, min_index = -1;
+
+            for (int v = 0; v < V; v++)
+                if (sptSet[v] == false && dist[v] <= min)
+                {
+                    min = dist[v];
+                    min_index = v;
+                }
+
+            return min_index;
+        }
+
+        // A utility function to print
+        // the constructed distance array
+        void printSolution(int[] dist, int n)
+        {
+            Console.Write("Вершина     Расстояние\n");
+            for (int i = 0; i < V; i++)
+                Console.Write(i + " \t\t " + dist[i] + "\n");
+        }
+
+        // Function that implements Dijkstra's
+        // single source shortest path algorithm
+        // for a graph represented using adjacency
+        // matrix representation
+        public void dijkstra(int[,] graph, int src)
+        {
+            int[] dist = new int[V]; // The output array. dist[i]
+                                     // will hold the shortest
+                                     // distance from src to i
+
+            // sptSet[i] will true if vertex
+            // i is included in shortest path
+            // tree or shortest distance from
+            // src to i is finalized
+            bool[] sptSet = new bool[V];
+
+            // Initialize all distances as
+            // INFINITE and stpSet[] as false
+            for (int i = 0; i < V; i++)
+            {
+                dist[i] = int.MaxValue;
+                sptSet[i] = false;
+            }
+
+            // Distance of source vertex
+            // from itself is always 0
+            dist[src] = 0;
+
+            // Find shortest path for all vertices
+            for (int count = 0; count < V - 1; count++)
+            {
+                // Pick the minimum distance vertex
+                // from the set of vertices not yet
+                // processed. u is always equal to
+                // src in first iteration.
+                int u = minDistance(dist, sptSet);
+
+                // Mark the picked vertex as processed
+                sptSet[u] = true;
+
+                // Update dist value of the adjacent
+                // vertices of the picked vertex.
+                for (int v = 0; v < V; v++)
+
+                    // Update dist[v] only if is not in
+                    // sptSet, there is an edge from u
+                    // to v, and total weight of path
+                    // from src to v through u is smaller
+                    // than current value of dist[v]
+                    if (!sptSet[v] && graph[u, v] != 0 &&
+                         dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+                        dist[v] = dist[u] + graph[u, v];
+            }
+
+            // print the constructed distance array
+            printSolution(dist, V);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deep-First Search algorithm
+            Graph g = new Graph(4);
+
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(1, 2);
+            g.AddEdge(2, 0);
+            g.AddEdge(2, 3);
+            g.AddEdge(3, 3);
+
+            Console.WriteLine(
+                "Поиск в глубину:");
+
+            g.DFS_method(2);
+
+            // Breadth-First Search algorithm
+            Graph g2 = new Graph(4);
+
+            g2.AddEdge(0, 1);
+            g2.AddEdge(0, 2);
+            g2.AddEdge(1, 2);
+            g2.AddEdge(2, 0);
+            g2.AddEdge(2, 3);
+            g2.AddEdge(3, 3);
+
+            Console.Write("Поиск в ширину:\n");
+            g2.BFS_method(2);
+
+            Console.WriteLine("Алгоритм Крускала:");
+            // Kruskal's algorithm
+            int verticesCount = 4;
+            int edgesCount = 5;
+            KruskalAlgorithm.Graph graph = KruskalAlgorithm.CreateGraph(verticesCount, edgesCount);
+
+            // Edge 0-1
+            graph.edge[0].Source = 0;
+            graph.edge[0].Destination = 1;
+            graph.edge[0].Weight = 10;
+
+            // Edge 0-2
+            graph.edge[1].Source = 0;
+            graph.edge[1].Destination = 2;
+            graph.edge[1].Weight = 6;
+
+            // Edge 0-3
+            graph.edge[2].Source = 0;
+            graph.edge[2].Destination = 3;
+            graph.edge[2].Weight = 5;
+
+            // Edge 1-3
+            graph.edge[3].Source = 1;
+            graph.edge[3].Destination = 3;
+            graph.edge[3].Weight = 15;
+
+            // Edge 2-3
+            graph.edge[4].Source = 2;
+            graph.edge[4].Destination = 3;
+            graph.edge[4].Weight = 4;
+
+            KruskalAlgorithm.Kruskal(graph);
+
+            // Dijkstra's algorithm
+            Console.WriteLine("Алгоритм Дейкстры:");
+            int[,] graph1 = new int[,] {
+                { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+            };
+            GFG t = new GFG();
+            t.dijkstra(graph1, 0);
+        }
+    }
+}
